7068f0a7719d6a5a75dcf81e2a6a62f4c84e8ebb
yulifrank <yaelfrank0338@gmail.com>
2023-11-29 19:26:47 +0200

    my commit message

--- myFirstApiYaelFrank/DataContext.cs
+++ myFirstApiYaelFrank/DataContext.cs
@@ -1,18 +1,27 @@
-﻿namespace myFirstApiYaelFrank
+﻿using CsvHelper;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace myFirstApiYaelFrank
 {
     public class DataContext: IDataContext
     {
 
-        public List<Event> EventList { get; set; }
+        public List<Event> EventList { get; set; } 
 
         //all lists - data from DB
         public DataContext()
         {
-            EventList = new List<Event>()
-            {  new Event { Id = Event.index, Title = "adi",Start=new DateTime(2023,10,25) },
-               new Event { Id = Event.index, Title = "noa",Start=new DateTime(2023,11,25) },
-               new Event { Id = Event.index, Title = "yael",Start=new DateTime(2023,11,22) }
-            };
+            //EventList = new List<Event>();
+            //{  new Event { Id = Event.index, Title = "adi",Start=new DateTime(2023,10,25) },
+            //   new Event { Id = Event.index, Title = "noa",Start=new DateTime(2023,11,25) },
+            //   new Event { Id = Event.index, Title = "yael",Start=new DateTime(2023,11,22) }
+            //};
+            using (var reader = new StreamReader("data.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                EventList = csv.GetRecords<Event>().ToList();
+            }
         }
 
     }
